9b7465e7869517d6c200c2b918637a8e7186da31
Agam Gupta <agamgupta2895@gmail.com>
2015-12-05 21:14:29 +0530

    make appointment file added

---
+++ app/src/main/java/com/bitslate/mrdoc/MakeAppointment.java
@@ -0,0 +1,14 @@
+package com.bitslate.mrdoc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MakeAppointment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_appointment);
+
+    }
+}
