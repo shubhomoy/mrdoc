80ba08b258ca5859016d4386aeee37034c921dde
Shubhakar <shubh.cool94@gmail.com>
2015-12-06 01:50:27 +0530

    UIChanges

--- app/src/main/java/com/bitslate/mrdoc/MrDocUtilities/Config.java
+++ app/src/main/java/com/bitslate/mrdoc/MrDocUtilities/Config.java
@@ -4,6 +4,6 @@ package com.bitslate.mrdoc.MrDocUtilities;
  * Created by vellapanti on 5/12/15.
  */
 public class Config {
-    public static final String coreUrl = "http://10.0.3.2:3000";
-    public static final String apiUrl = "http://10.0.3.2:3000/api";
+    public static final String coreUrl = "http://10.0.2.2:3000";
+    public static final String apiUrl = "http://10.0.2.2:3000/api";
 }
